5cbfb244f4f87b8a3f07806c31d4bf2c9bb3f787
xaviguasch <xaviguasch@gmail.com>
2019-04-23 23:25:36 +0200

    added dynamic background via Particles package

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Particles from 'react-particles-js'
 import './App.css'
 
-function App() {
-  return (
-    <div className='App'>
-      <Navigation />
-      <Logo />
-      <Rank />
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition />*/}
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+
+        <ImageLinkForm />
+        {/*
+        <FaceRecognition />*/}
+      </div>
+    )
+  }
 }
 
 export default App
