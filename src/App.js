6fbdfed9691f1a417169d17fdfd4b86cfe2667b8
xaviguasch <xaviguasch@gmail.com>
2019-04-24 11:58:16 +0200

    added clarifai API to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import Particles from 'react-particles-js'
+import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import Particles from 'react-particles-js'
 import './App.css'
+const API_KEY = process.env.REACT_APP_CLARIFAI_API_KEY
+
+const app = new Clarifai.App({
+  apiKey: API_KEY
+})
 
 const particlesOptions = {
   particles: {
@@ -32,6 +38,22 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click')
+
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        'https://samples.clarifai.com/metro-north.jpg'
+      )
+      .then(
+        function(response) {
+          console.log(response)
+
+          // do something with responseconsole.log(response);
+        },
+        function(err) {
+          // there was an error
+        }
+      )
   }
 
   render() {
