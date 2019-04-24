b51e89ac1f3060684c76e49ad80c842f981451c6
xaviguasch <xaviguasch@gmail.com>
2019-04-24 11:31:19 +0200

    added onInputChange and onButtonSubmit functions

--- src/App.js
+++ src/App.js
@@ -19,6 +19,21 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+  }
+
   render() {
     return (
       <div className='App'>
@@ -27,7 +42,10 @@ class App extends Component {
         <Logo />
         <Rank />
 
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/*
         <FaceRecognition />*/}
       </div>
