b0099614ee4e4dc764335e51ba7533c77ff35bd6
xaviguasch <xaviguasch@gmail.com>
2019-04-24 13:18:08 +0200

    extracting face date from the clarifai API

--- src/App.js
+++ src/App.js
@@ -41,9 +41,11 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input })
 
-    app.models.predict(Clarifai.COLOR_MODEL, this.state.input).then(
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
       function(response) {
-        console.log(response)
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        )
 
         // do something with responseconsole.log(response);
       },
