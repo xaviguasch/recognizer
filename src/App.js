6fe508fc4214cbe1c8c0fcfa0278649c601de4cd
xaviguasch <xaviguasch@gmail.com>
2019-04-24 16:03:13 +0200

    extracted the face coordenates detected on the images and put it in a object

--- src/App.js
+++ src/App.js
@@ -30,8 +30,29 @@ class App extends Component {
     super()
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = data => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    }
+  }
+
+  displayFaceBox = box => {
+    console.log(box)
+
+    this.setState({ box })
   }
 
   onInputChange = event => {
@@ -41,18 +62,10 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input })
 
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        )
-
-        // do something with responseconsole.log(response);
-      },
-      function(err) {
-        // there was an error
-      }
-    )
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -63,10 +76,7 @@ class App extends Component {
         <Logo />
         <Rank />
 
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
         <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     )
