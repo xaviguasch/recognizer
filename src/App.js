fe8e53eb78ee22258fd58677c657572e4a652973
xaviguasch <xaviguasch@gmail.com>
2019-04-24 12:58:43 +0200

    implemented logic for image recognition

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import './App.css'
 const API_KEY = process.env.REACT_APP_CLARIFAI_API_KEY
 
@@ -28,32 +29,28 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = event => {
-    console.log(event.target.value)
+    this.setState({ input: event.target.value })
   }
 
   onButtonSubmit = () => {
-    console.log('click')
+    this.setState({ imageUrl: this.state.input })
 
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        'https://samples.clarifai.com/metro-north.jpg'
-      )
-      .then(
-        function(response) {
-          console.log(response)
+    app.models.predict(Clarifai.COLOR_MODEL, this.state.input).then(
+      function(response) {
+        console.log(response)
 
-          // do something with responseconsole.log(response);
-        },
-        function(err) {
-          // there was an error
-        }
-      )
+        // do something with responseconsole.log(response);
+      },
+      function(err) {
+        // there was an error
+      }
+    )
   }
 
   render() {
@@ -68,8 +65,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/*
-        <FaceRecognition />*/}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     )
   }
