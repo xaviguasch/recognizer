216a362168fc4a1323f3df5db1fef3f45be67250
xaviguasch <xaviguasch@gmail.com>
2019-04-25 12:02:04 +0200

    added conditional logic to Signin

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
+      route: 'signin'
     }
   }
 
@@ -67,16 +68,26 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
+  onRouteChange = () => {
+    this.setState({ route: 'home' })
+  }
+
   render() {
     return (
       <div className='App'>
         <Particles className='particles' params={particlesOptions} />
-        <Signin />
         <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+
+        {this.state.route === 'signin' ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+          </div>
+        )}
       </div>
     )
   }
