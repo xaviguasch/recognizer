fa4eda579b1e4cce932e52c93f64a039df42a9f6
xaviguasch <xaviguasch@gmail.com>
2019-04-25 14:01:12 +0200

    cleaning up the app and solving small bugs

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Signin from './components/Signin/Signin'
+import Register from './components/Register/Register'
 import './App.css'
 const API_KEY = process.env.REACT_APP_CLARIFAI_API_KEY
 
@@ -33,7 +34,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -69,24 +71,32 @@ class App extends Component {
   }
 
   onRouteChange = route => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
     this.setState({ route: route })
   }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state
     return (
       <div className='App'>
         <Particles className='particles' params={particlesOptions} />
-        <Navigation onRouteChange={this.onRouteChange} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
 
-        {this.state.route === 'signin' ? (
-          <Signin onRouteChange={this.onRouteChange} />
-        ) : (
+        {route === 'home' ? (
           <div>
             <Logo />
             <Rank />
             <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
+        ) : route === 'signin' || route === 'signout' ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
         )}
       </div>
     )
