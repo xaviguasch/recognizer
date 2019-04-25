2f443aab24b8a31d6b67888fb698f2b0a7311ffc
xaviguasch <xaviguasch@gmail.com>
2019-04-25 12:13:21 +0200

    signed in and out going to each route

--- src/App.js
+++ src/App.js
@@ -68,15 +68,15 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
-  onRouteChange = () => {
-    this.setState({ route: 'home' })
+  onRouteChange = route => {
+    this.setState({ route: route })
   }
 
   render() {
     return (
       <div className='App'>
         <Particles className='particles' params={particlesOptions} />
-        <Navigation />
+        <Navigation onRouteChange={this.onRouteChange} />
 
         {this.state.route === 'signin' ? (
           <Signin onRouteChange={this.onRouteChange} />
